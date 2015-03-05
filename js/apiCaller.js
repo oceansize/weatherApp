81fc06f65b6b441916613977d4cef41bc6e3895a
Roi Driscoll <roi.driscoll@gmail.com>
2015-03-05 15:41:51 +0000

    Select working, styling updated

--- js/apiCaller.js
+++ js/apiCaller.js
@@ -2,16 +2,29 @@ console.log("They're coming to get you Barbara...");
 
 $(document).ready(function() {
 
-  var city = 'mexico_city'
-  var data = $.ajax({
-    dataType: "jsonp",
-    url: 'http://weather-api.herokuapp.com/temperature?city=' + city,
-    success: function(response ) {
-      console.log( data.responseJSON ); // server response
-      $('.temperature').text(data.responseJSON.temp);
-      $('img').attr("src", 'images/' + data.responseJSON.outlook + '.svg');
-      $('#icon').removeClass().addClass(data.responseJSON.outlook);
-    }
+  $.ajax({
+    url: 'http://weather-api.herokuapp.com/cities',
+    dataType: "jsonp"
+  }).done(function(cityList) {
+      $.each(cityList.cities, function(index, city) {
+        $('.cities').append(
+          $('<option></option>').val(city).html(city));
+      });
+    });
+
+  $('select').change(function() {
+    var city = $( "select option:selected" ).val();
+    console.log(city);
+    var data = $.ajax({
+      dataType: "jsonp",
+      url: 'http://weather-api.herokuapp.com/temperature?city=' + city,
+      success: function(response ) {
+        console.log( data.responseJSON ); // server response
+        $('.temperature').text(data.responseJSON.temp);
+        $('img').attr("src", 'images/' + data.responseJSON.outlook + '.svg');
+        $('#icon').removeClass().addClass(data.responseJSON.outlook);
+      }
+    });
   });
 
 });
