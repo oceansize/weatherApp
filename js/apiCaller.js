f5ecb97af5225d0815d2232a287ad6dacc05ad1e
Roi Driscoll <roi.driscoll@gmail.com>
2015-03-06 19:15:29 +0000

    going home

--- js/apiCaller.js
+++ js/apiCaller.js
@@ -1,31 +1,47 @@
 console.log("They're coming to get you Barbara...");
 
+var cityListApi = 'http://weather-api.herokuapp.com/cities';
+var individualCityApi = 'http://weather-api.herokuapp.com/temperature?city=';
+
 $(document).ready(function() {
 
-  $.ajax({
-    url: 'http://weather-api.herokuapp.com/cities',
-    dataType: "jsonp"
-  }).done(function(cityList) {
-      $.each(cityList.cities, function(index, city) {
-        $('.cities').append(
-          $('<option></option>').val(city).html(city));
-      });
+  var populateSelect = function(cityList) {
+    $.each(cityList.cities, function(index, city) {
+      $('.cities').append(
+        $('<option></option>').val(city).html(city));
     });
+  };
+
+  var retrieveCities = function() {
+    $.ajax({
+      url: cityListApi,
+      dataType: 'jsonp'
+    }).done(function(cityList) {
+      populateSelect(cityList);
+      getIndividualCity(cityList.cities[0]);
+    });
+  };
+
+  var updateStyling = function(individualCity){
+    $('.temperature').text(individualCity.temp);
+    console.log(individualCity);
+    $('img').attr('src', 'images/' + individualCity.outlook + '.svg');
+    $('#icon').attr('class',individualCity.outlook);
+    $('body').attr('class',individualCity.outlook + '_wallpaper');
+  };
+
+  var getIndividualCity = function(city) {
+    $.ajax({
+      dataType: 'jsonp',
+      url: individualCityApi + city,
+      success: function(individualCity) { updateStyling(individualCity); }
+    });
+  };
 
   $('select').change(function() {
-    var city = $( "select option:selected" ).val();
-    console.log(city);
-    var data = $.ajax({
-      dataType: "jsonp",
-      url: 'http://weather-api.herokuapp.com/temperature?city=' + city,
-      success: function(response ) {
-        console.log( data.responseJSON ); // server response
-        $('.temperature').text(data.responseJSON.temp);
-        $('img').attr("src", 'images/' + data.responseJSON.outlook + '.svg');
-        $('#icon').removeClass().addClass(data.responseJSON.outlook);
-        $('body').removeClass().addClass(data.responseJSON.outlook + '_wallpaper');
-      }
-    });
+    var city = $( 'select option:selected' ).val();
+    getIndividualCity(city);
   });
 
+  retrieveCities();
 });
